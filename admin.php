8e148f9a36506ac39902148caaca80c979d219ac
tomasztaw <tomasz.wegrzyn.wro@gmail.com>
2023-11-18 15:19:27 +0100

    Koniec filmu, przed zadaniem domowym

--- admin.php
+++ admin.php
@@ -1,9 +1,14 @@
 <?php
 session_start();
 
+if (isset($_SESSION['logged_id'])) {
+    header('Location: list.php');
+    exit();
+}
 ?>
 <!DOCTYPE html>
 <html lang="pl">
+
 <head>
     <meta charset="utf-8">
     <title>Panel administracyjny</title>
@@ -12,7 +17,8 @@ session_start();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
     <link rel="stylesheet" href="main.css">
-    <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700&amp;subset=latin-ext" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Lobster|Open+Sans:400,700&amp;subset=latin-ext"
+        rel="stylesheet">
     <!--[if lt IE 9]>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
     <![endif]-->
@@ -31,10 +37,17 @@ session_start();
                     <label>Login <input type="text" name="login"></label>
                     <label>Hasło <input type="password" name="pass"></label>
                     <input type="submit" value="Zaloguj się!">
+                    <?php
+                        if (isset($_SESSION['bad_attempt'])) {
+                            echo '<p>Niepoprawny lgoin lub hasło!</p>';
+                            unset($_SESSION['bad_attempt']);
+                        }
+                    ?>
                 </form>
             </article>
         </main>
 
     </div>
 </body>
+
 </html>
